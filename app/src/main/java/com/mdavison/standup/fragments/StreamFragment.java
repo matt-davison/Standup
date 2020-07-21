ff27a721317acced01aadd154da56217e6900f55
Matt Davison <mdavison@fb.com>
2020-07-21 01:07:33 -0400

    Add swipe to vote for home feed

--- app/src/main/java/com/mdavison/standup/fragments/StreamFragment.java
+++ app/src/main/java/com/mdavison/standup/fragments/StreamFragment.java
@@ -2,15 +2,14 @@ package com.mdavison.standup.fragments;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,12 +43,12 @@ public class StreamFragment extends Fragment {
     private static final String TAG = "StreamFragment";
     //TODO: Switch to queue
     private List<Post> posts;
-    private CardView post;
+    private CardView postFront;
     private CardView postBehind;
-    private TextView tvTitle;
-    private TextView tvAuthor;
-    private ImageView ivMedia;
+
     private ParseUser currentUser;
+
+    private int postsViewed = 0;
 
     public StreamFragment() {
         // Required empty public constructor
@@ -64,116 +63,131 @@ public class StreamFragment extends Fragment {
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
-    public void onViewCreated(@NonNull View view,
+    public void onViewCreated(@NonNull final View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         posts = new LinkedList<Post>();
         queryPosts();
-        tvTitle = view.findViewById(R.id.tvTitle);
-        tvAuthor = view.findViewById(R.id.tvAuthor);
-        ivMedia = view.findViewById(R.id.ivMedia);
 
-        //TODO: Remove this button and use swipe gesture instead
-        final Button btnLike = view.findViewById(R.id.btnLike);
-        btnLike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                likePost();
-            }
-        });
         postBehind = view.findViewById(R.id.postBehind);
-        postBehind.setVisibility(View.GONE);
-        post = view.findViewById(R.id.post);
-        CardView postCard = post.findViewById(R.id.cvPost);
-        /*
-        post.setOnTouchListener(new OnSwipeTouchListener(getContext()) {
+
+        postFront = view.findViewById(R.id.post);
+
+        postFront.setOnTouchListener(new OnSwipeTouchListener(getContext()) {
 
             @Override
             public void onSwipeRight() {
-                Toast.makeText(getContext(), "\u2764", Toast.LENGTH_SHORT).show();
-                likePost();
+                Toast.makeText(getContext(), "\uD83D\uDC4D", Toast.LENGTH_SHORT)
+                        .show();
+                ValueAnimator swipeAnim = ObjectAnimator
+                        .ofFloat(postFront, View.TRANSLATION_X, view.getWidth());
+                swipeAnim.setDuration(250);
+                swipeAnim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        likePost();
+                        ValueAnimator returnAnim = ObjectAnimator
+                                .ofFloat(postFront, View.X, 0f);
+                        returnAnim.setDuration(0);
+                        returnAnim.start();
+                    }
+                });
+                swipeAnim.start();
+                Log.i(TAG, "swipe right");
             }
 
             @Override
             public void onSwipeLeft() {
-                viewPost();
+                Toast.makeText(getContext(), "\uD83D\uDC4E", Toast.LENGTH_SHORT).show();
+                ValueAnimator swipeAnim = ObjectAnimator
+                        .ofFloat(postFront, View.TRANSLATION_X, -view.getWidth());
+                swipeAnim.setDuration(250);
+                swipeAnim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        viewPost();
+                        ValueAnimator returnAnim = ObjectAnimator
+                                .ofFloat(postFront, View.X, 0f);
+                        returnAnim.setDuration(0);
+                        returnAnim.start();
+                    }
+                });
+                swipeAnim.start();
+                Log.i(TAG, "swipe left");
             }
 
-        });
-        */
-
-        postCard.setOnTouchListener(new View.OnTouchListener() {
-            float dx, dy;
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch (motionEvent.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.i(TAG, "Pressing");
-                        dx = view.getX() - motionEvent.getRawX();
-                        dy = view.getY() - motionEvent.getRawY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        Log.i(TAG, "Moving");
-                        view.animate()
-                                .x(motionEvent.getRawX() + dx - (view.getWidth() / 2))
-                                .y(motionEvent.getRawY() + dy - (view.getHeight() / 2))
-                                .setDuration(0)
-                                .start();
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        if (motionEvent.getRawX() >= view.getWidth() * 4 / 5) {
-                            likePost();
-                        }
-                        else if (motionEvent.getRawX() <= view.getWidth() / 5) {
-                            viewPost();
-                        }
-                        view.clearAnimation();
-                        view.animate().translationX(0).translationY(0);
-                    default:
-                        return false;
-                }
-                return true;
-            }
-        });
-        post.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "click");
-            }
         });
     }
 
     private void likePost() {
+        if (posts.size() == 0) {
+            return;
+        }
         posts.get(0).addLike(currentUser);
         viewPost();
     }
 
     private void viewPost() {
+        if (posts.size() == 0) {
+            return;
+        }
         posts.get(0).addViewer(currentUser);
+        postsViewed++;
         advancePost();
     }
 
     private void advancePost() {
+        if (posts.size() == 0) {
+            Log.e(TAG, "No posts to advance");
+            makeText(getContext(), "No posts!", Toast.LENGTH_LONG)
+                    .show();
+            //queryPosts();
+            return;
+        }
+        if (posts.size() < 5) {
+            queryPosts();
+        }
         posts.remove(0);
-        showPost();
+        setPosts();
     }
 
-    private void showPost() {
+    private void setPosts() {
         if (posts.size() == 0) {
             Log.e(TAG, "No posts to show");
             makeText(getContext(), "No posts to show", Toast.LENGTH_LONG)
                     .show();
             return;
         }
-        tvTitle.setText(posts.get(0).getTitle());
-        if (posts.get(0).getMedia() != null) {
-            Glide.with(getContext()).load(posts.get(0).getMedia().getUrl())
+
+        if (posts.size() == 1) {
+            Log.e(TAG, "Not enough posts");
+            makeText(getContext(), "Last Post", Toast.LENGTH_LONG)
+                    .show();
+            setPost(posts.get(0), postFront);
+            return;
+        }
+
+        setPost(posts.get(0), postFront);
+        setPost(posts.get(1), postBehind);
+    }
+    private void setPost(Post post, CardView postView) {
+
+        TextView tvTitle = postView.findViewById(R.id.tvTitle);
+        TextView tvAuthor = postView.findViewById(R.id.tvAuthor);
+        ImageView ivMedia = postView.findViewById(R.id.ivMedia);
+
+        tvTitle.setText(post.getTitle());
+        Log.i(TAG, "attaching: " + post.getTitle());
+        if (post.getMedia() != null) {
+            Glide.with(getContext()).load(post.getMedia().getUrl())
                     .into(ivMedia);
         }
         //Change fetchIfNeeded to query.include(ptr to author) to reduce queries
         try {
             tvAuthor.setText(
-                    (posts.get(0).getAuthor().fetchIfNeeded()).getUsername());
+                    (post.getAuthor().fetchIfNeeded()).getUsername());
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -191,8 +205,7 @@ public class StreamFragment extends Fragment {
                                      ParseException e) {
                         if (e != null) {
                             Log.e(TAG, "Issues with getting communities", e);
-                            makeText(getContext(),
-                                    "Issue getting communities",
+                            makeText(getContext(), "Issue getting communities",
                                     Toast.LENGTH_SHORT).show();
                             return;
                         }
@@ -215,6 +228,7 @@ public class StreamFragment extends Fragment {
                         //do similar for following posts
                         ParseQuery<Post> query = ParseQuery.or(queries);
                         query.setLimit(20);
+                        query.setSkip(postsViewed);
                         query.addDescendingOrder(Post.KEY_CREATED);
                         query.findInBackground(new FindCallback<Post>() {
                             @Override
@@ -230,7 +244,9 @@ public class StreamFragment extends Fragment {
                                 posts.addAll(newPosts);
                                 Log.i(TAG,
                                         "Received " + posts.size() + " posts");
-                                showPost();
+                                if (postsViewed == 0) {
+                                    setPosts();
+                                }
                             }
                         });
                     }
