6930c2bc2b83225be112248a13ad0bbe6574d35f
Matt Davison <mdavison@fb.com>
2020-07-23 16:35:58 -0400

    Add basic camera use and photo selection

--- app/src/main/java/com/mdavison/standup/fragments/ComposeFragment.java
+++ app/src/main/java/com/mdavison/standup/fragments/ComposeFragment.java
@@ -7,19 +7,10 @@ import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
-import androidx.fragment.app.Fragment;
-
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -27,11 +18,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
 import com.mdavison.standup.R;
-import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import java.io.File;
 import java.io.IOException;
@@ -45,12 +38,13 @@ import static android.app.Activity.RESULT_OK;
  */
 public class ComposeFragment extends Fragment {
 
+    public final static int PICK_PHOTO_CODE = 8;
     private static final String TAG = "ComposeFragment";
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 7;
-    public final static int PICK_PHOTO_CODE = 8;
     private static final String PHOTO_FILENAME = "photo.jpg";
     private File photoFile;
     private ImageView ivPostImage;
+    private ParseUser currentUser;
 
     public ComposeFragment() {
         // Required empty public constructor
@@ -73,7 +67,14 @@ public class ComposeFragment extends Fragment {
         final Button btnCaptureImage = view.findViewById(R.id.btnCaptureImage);
         ivPostImage = view.findViewById(R.id.ivPostImage);
         final Button btnSubmit = view.findViewById(R.id.btnSubmit);
-
+        final Button btnSelectImage = view.findViewById(R.id.btnSelectImage);
+        currentUser = ParseUser.getCurrentUser();
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickPhoto();
+            }
+        });
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,8 +96,7 @@ public class ComposeFragment extends Fragment {
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(description, currentUser, photoFile);
+                savePost();
             }
         });
     }
@@ -117,22 +117,29 @@ public class ComposeFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             }
         }
+        if (requestCode == PICK_PHOTO_CODE && data != null) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+
+            // Load the selected image into a preview
+            ivPostImage.setImageBitmap(selectedImage);
+        }
     }
 
     private void launchCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         photoFile = getPhotoFileUri(PHOTO_FILENAME);
-        Uri fileProvider = FileProvider
-                .getUriForFile(getContext(), "com.codepath.fileprovider",
-                        photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(getContext(),
+                "com.codepath.fileprovider.standup", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
         if (intent.resolveActivity(getContext().getPackageManager()) != null) {
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
     }
 
-    private void savePost(String description, ParseUser currentUser,
-                          File photoFile) {
+    private void savePost() {
         //save the post to Parse
     }
 
@@ -151,16 +158,16 @@ public class ComposeFragment extends Fragment {
         return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
-    /*
     // Trigger gallery selection for a photo
-    public void onPickPhoto(View view) {
+    public void onPickPhoto() {
         // Create intent for picking a photo from the gallery
         Intent intent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // If you call startActivityForResult() using an intent that no app
+        // can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
             // Bring up gallery to select a photo
             startActivityForResult(intent, PICK_PHOTO_CODE);
         }
@@ -170,32 +177,20 @@ public class ComposeFragment extends Fragment {
         Bitmap image = null;
         try {
             // check version of Android on device
-            if(Build.VERSION.SDK_INT > 27){
+            if (Build.VERSION.SDK_INT > 27) {
                 // on newer versions of Android, use the new decodeBitmap method
-                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
+                ImageDecoder.Source source = ImageDecoder
+                        .createSource(getContext().getContentResolver(),
+                                photoUri);
                 image = ImageDecoder.decodeBitmap(source);
             } else {
                 // support older versions of Android by using getBitmap
-                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+                image = MediaStore.Images.Media
+                        .getBitmap(getContext().getContentResolver(), photoUri);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return image;
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
-            Uri photoUri = data.getData();
-
-            // Load the image located at photoUri into selectedImage
-            Bitmap selectedImage = loadFromUri(photoUri);
-
-            // Load the selected image into a preview
-            ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
-            ivPreview.setImageBitmap(selectedImage);
-        }
-    }
-     */
 }
