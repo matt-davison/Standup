24ddb7a03af9ce4fb0a46a135384ae9b399e7a41
Matt Davison <mdavison@fb.com>
2020-07-24 18:25:11 -0400

    Add logout

--- app/src/main/java/com/mdavison/standup/fragments/ProfileFragment.java
+++ app/src/main/java/com/mdavison/standup/fragments/ProfileFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.mdavison.standup.R;
+import com.parse.ParseUser;
 
 
 public class ProfileFragment extends Fragment {
@@ -29,6 +31,13 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        ImageView ivLogout = view.findViewById(R.id.ivLogout);
+        ivLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                getActivity().finish();
+            }
+        });
     }
 }
